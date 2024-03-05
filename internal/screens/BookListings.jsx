7028d11d886b072451fca3336202b3b5c2cdfd6c
amon1te <alenshakeyev@gmail.com>
2024-03-06 02:39:44 +0500

    connect

---
+++ internal/screens/BookListings.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const BookListings = () => {
+    const URL = "https://openlibrary.org/search.json";
+    return (
+        <View>
+            <Text>Kentik</Text>
+        </View>
+    );
+};
+
+export default BookListings;
+
+const styles =  StyleSheet.create({});
